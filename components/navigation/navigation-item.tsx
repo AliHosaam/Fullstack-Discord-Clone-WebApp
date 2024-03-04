910d205a96dacb25187d6c207febb4ac9b615c30
AliHosaam <ah5778902@gmail.com>
2024-03-04 05:49:21 +0300

    last

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -34,15 +34,15 @@ const NavigationItem: React.FC<NavigationItemProps> = ({
           <div
             className={`
               absolute left-0 bg-black dark:bg-white rounded-r-full transition-all w-[4px]
-              ${params?.serverId !== id && "group-hover:h-[20px]"}
-              ${params?.serverId === id ? "h-[36px]" : "h-[8px]"}
+              ${params.serverId !== id && "group-hover:h-[20px]"}
+              ${params.serverId === id ? "h-[36px]" : "h-[8px]"}
             `}
           />
           <div
             className={`
               relative group flex h-[48px] w-[48px] ml-3 rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden
               ${
-                params?.serverId === id &&
+                params.serverId === id &&
                 "bg-black/10 text-black dark:text-white dark:bg-white/10 rounded-[16px]"
               }
             `}
